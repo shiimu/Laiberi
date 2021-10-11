32ade0be416c06792c5c5f8d8a2fb4cebc391e45
Siim Laineste <siim.laineste@outlook.com>
2021-10-11 10:48:15 +0300

    2 classes - Available and Borrowed books
    In theory it would go
    
    if borrowed -> rm available -> mk borrowed.

--- LaiberiAPI/main.py
+++ LaiberiAPI/main.py
@@ -11,7 +11,7 @@ def index():
     return "Allooo"
 
 
-class Name(Resource):
+class Available(Resource):
     def get(self):
         client = MongoClient('localhost', 27017)
         db = client['Libari']
@@ -83,7 +83,80 @@ class Name(Resource):
     def push(self):
         return 401
 
-api.add_resource(Name, '/name')
+class Borrowed(Resource):
+    def get(self):
+        client = MongoClient('localhost', 27017)
+        db = client['Libari']
+        collection = db['booksBorrow']
+        # had to make id not show, because it threw a not json serializable error.
+        retrieved = list(collection.find({}, {'_id' : False}))
+        return retrieved, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required = True)
+        parser.add_argument('writer', required = True)
+        parser.add_argument('year', required = True)
+        parser.add_argument('isbn', required = True)
+        parser.add_argument('rating', required = True)
+        parser.add_argument('about', required = True)
+        parser.add_argument('tags', required = True)
+        parser.add_argument('description', required = True)
+
+        args = parser.parse_args()
+
+        client = MongoClient('localhost', 27017)
+        db = client['Libari']
+        collection = db['booksBorrow']
+        new_book = collection.insert_one({
+            'Name' : args['name'],
+            'Writer' : args['writer'],
+            'Year' : args['year'],
+            'ISBN' : args['isbn'],
+            'Rating' : args['rating'],
+            'About' : args['about'],
+            'Tags' : args['tags'],            
+            'Description' : args['description']        
+        })    
+        retrieved = list(collection.find({}, {'_id' : False}))
+        return retrieved, 200
+    
+    def put(self):
+        return 401
+
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required = True)
+        parser.add_argument('writer', required = True)
+        parser.add_argument('year', required = True)
+        parser.add_argument('isbn', required = True)
+        parser.add_argument('rating', required = True)
+        parser.add_argument('about', required = True)
+        parser.add_argument('tags', required = True)
+        parser.add_argument('description', required = True)
+        args = parser.parse_args()
+
+        client = MongoClient('localhost', 27017)
+        db = client['Libari']
+        collection = db['booksBorrow']
+        removeBook = collection.find_one_and_delete({
+            'Name' : args['name'],
+            'Writer' : args['writer'],
+            'Year' : args['year'],
+            'ISBN' : args['isbn'],
+            'Rating' : args['rating'],
+            'About' : args['about'],
+            'Description' : args['description']        
+        })    
+        retrieved = list(collection.find({}, {'_id' : False}))
+        return retrieved, 200
+        
+
+    def push(self):
+        return 401
+
+api.add_resource(Available, '/available')
+api.add_resource(Borrowed, '/borrowed')
 
 
 if __name__ == "__main__":
